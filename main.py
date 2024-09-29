debb54aa35f14c56fcfc1fa1c7f49487a565bed7
ahiehe <127009820+ahiehe@users.noreply.github.com>
2024-09-29 09:47:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,31 @@ async def gpass(ctx, *args):
 async def repeat(ctx, content, times = 5):
     for i in range(times):
         await ctx.send(content)
+        
+@bot.command()
+async def randmem(ctx):
+    picture = "images/" + random.choice(os.listdir("images"))
+    with open(picture, 'rb') as mem:
+        picture = discord.File(mem)
+    await ctx.send(file=picture)
 
+def get_rand_image_url(url, imageKeyjson):    
+    res = requests.get(url)
+    data = res.json()
+    return data[imageKeyjson]
+
+
+@bot.command('duck')
+async def duck(ctx):
+
+    image_url = get_rand_image_url('https://random-d.uk/api/random', 'url')
+    await ctx.send(image_url)
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_rand_image_url('https://some-random-api.com/animal/dog', 'image')
+    await ctx.send(image_url)
+    
 @bot.command()
 async def joined(ctx, member: discord.Member):
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
